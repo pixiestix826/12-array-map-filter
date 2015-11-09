4c59e56a25da99e4b0ec7b68c3250ab27f6ece21
Brandi Soucy <bbsoucy@gmail.com>
2015-11-08 22:52:15 -0600

    Moving variable so they are defined

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 //calculate the average price of all items
 //console.log the average price
 function AvgPrice() {
-  var price = [];
   return items.map(function(item) {
     return item.price;
   });
 }
 
+var price = [];
 console.log(price);
