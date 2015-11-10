20e37ab40f37210bfdc853c72520db3834f52d0e
Brandi Soucy <bbsoucy@gmail.com>
2015-11-09 22:50:43 -0600

    attempting challenge 4

--- public/main.js
+++ public/main.js
@@ -31,4 +31,14 @@ var euroItem = items.filter((item) => {
 
 console.log(euroItem);
 
-//
+//find the items made of wood
+//console.log those items
+var woodenItems = items.filter((item) => {
+  if (item.materials) {
+    return item.materials === 'wood';
+  }
+}).map(function(item) {
+  return item.title;
+});
+
+console.log(woodenItems, ' is made of wood.');
